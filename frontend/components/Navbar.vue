dd018a290c8bbe055b08ea54baad99f94a40506a
tientxgreenwich <tien.rcvn@greenwich.co.jp>
2021-12-16 15:37:21 +0700

    update css for menu

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -1,39 +1,42 @@
 <template >
-	<nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
-		<nuxt-link to="/users" class="navbar-brand">Frontend</nuxt-link>
-		<button
-			class="navbar-toggler"
-			type="button"
-			data-toggle="collapse"
-			data-target="#navbarNav"
-			aria-controls="navbarNav"
-			aria-expanded="false"
-			aria-label="Toggle navigation"
-		>
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="navbarNav">
-			<ul class="navbar-nav">
-				<li class="nav-item active">
-					<nuxt-link to="/users" class="nav-link">User</nuxt-link>
-				</li>
-				<li class="nav-item active">
-					<nuxt-link to="/products" class="nav-link"
-						>Product</nuxt-link
-					>
-				</li>
-			</ul>
-			<template v-if="authenticated">
-				<ul class="navbar-nav ml-auto">
-					<li class="nav-item">
-						<a class="nav-link">Hi {{ user.name }}</a>
-					</li>
-					<li class="nav-item">
-						<a @click.prevent="logout" class="nav-link" href="#">Logout</a>
-					</li>
-				</ul>
-			</template>
-		</div>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nuxt-link to="/users" class="navbar-brand">
+            <img src="~/assets/images/rcvn_lg.png" alt="Rivercrane Logo" class="brand-image img-circle elevation-3" style="opacity: .8; border-radius: 50%; height: 30px">
+            <span class="brand-text font-weight-bold">RiverCrane Vietnam</span>
+        </nuxt-link>
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <nuxt-link to="/users" class="nav-link">User</nuxt-link>
+                </li>
+                <li class="nav-item active">
+                    <nuxt-link to="/products" class="nav-link"
+                        >Product</nuxt-link
+                    >
+                </li>
+            </ul>
+            <template v-if="authenticated">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link">Hi {{ user.name }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <a @click.prevent="logout" class="nav-link" href="#">Logout</a>
+                    </li>
+                </ul>
+            </template>
+        </div>
 	</nav>
 </template>
 <script>
--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,8 +1,18 @@
 <template>
-  <div>
-    <Navbar/>
-    <nuxt/>
-  </div>
+    <div>
+        <Navbar/>
+        <div class="content-wrapper">
+            <!-- Content Wrapper. Contains page content -->
+            <div class="content-wrapper">
+                <!-- Content Header (Page header) -->
+                <div class="content-header mb-5">                    
+                </div>
+                <div class="content">
+                    <nuxt/>
+                </div>
+            </div>
+        </div>    
+    </div>
 </template>
 
 <script>
